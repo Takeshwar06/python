bd55fc37ea9bdb2fc8d49b51ff5729365c4dfdda
takeshwar <takeshwarverma124@gmail.com>
2023-12-28 22:06:28 +0530

    basics of matplotlib

---
+++ Matplotlib/basicMatplotlib.py
@@ -0,0 +1,11 @@
+# first you need to install matplotlib
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+print(matplotlib.__version__)
+
+xpoints = np.array([0, 6])
+ypoints = np.array([0, 250])
+
+plt.plot(xpoints, ypoints)
+plt.show()
